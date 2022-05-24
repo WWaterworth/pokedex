4bbeb013c456c097f2882c1ec9a9fd485376b461
Warren Waterworth <warren.waterworth@gmail.com>
2022-05-23 19:35:15 -0600

    dropdown grabs all pokemon. need to pass url to fetchSinglePokemon

--- src/api/index.js
+++ src/api/index.js
@@ -2,9 +2,19 @@ export const fetchPokemon = async (url) => {
   try {
     const response = await fetch(url);
     const data = await response.json();
-    console.log(data);
     return data;
   } catch (error) {
     throw error;
+  }
+};
+
+export const fetchSinglePokemon = async (url) => {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log("single pokemon from api", data);
+    return data;
+  } catch (error) {
+    console.log(error);
   }
 };
--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,47 @@
-import { fetchPokemon } from "../api";
+import { fetchPokemon, fetchSinglePokemon } from "../api";
+import { useState, useEffect } from "react";
 
-function App() {
-  fetchPokemon("https://pokeapi.co/api/v2/pokemon/");
+const App = () => {
+  const [allPokemon, setPokemon] = useState([]);
+  const [singlePokemon, setSinglePokemon] = useState([]);
+  const [selectedPokemon, setSelectedPokemon] = useState("");
+  const [url, setUrl] = useState("");
+
+  useEffect(() => {
+    const getAllPokemon = async () => {
+      const getPokemon = await fetchPokemon(
+        "https://pokeapi.co/api/v2/pokemon/?limit=151"
+      );
+      console.log("results from getPokemon", getPokemon.results);
+      setPokemon(getPokemon.results);
+    };
+    getAllPokemon();
+  }, [setPokemon]);
+
+  const handleSelect = async (event) => {
+    setSelectedPokemon(event.target.value);
+    const singlePoke = await fetchSinglePokemon(
+      `https://pokeapi.co/api/v2/pokemon/20/`
+    );
+    setSinglePokemon(singlePoke);
+  };
+  console.log("single pokemon state", singlePokemon);
+
   return (
     <>
       <h1>Welcome to the Pokedex</h1>
+      <select onChange={handleSelect}>
+        {allPokemon.map((poke) => {
+          return (
+            <>
+              <option>{poke.name}</option>
+            </>
+          );
+        })}
+      </select>
+      <h2>Selected Pokemon: {selectedPokemon}</h2>
     </>
   );
-}
+};
 
 export default App;
