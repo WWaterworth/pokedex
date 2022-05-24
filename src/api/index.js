22f1163a1b6c7474036cbb86f151d401cb8bf6f7
Warren Waterworth <warren.waterworth@gmail.com>
2022-05-23 21:13:57 -0600

    sprite images shown. Pokemon 1 set to default to match dropdown

--- src/api/index.js
+++ src/api/index.js
@@ -12,9 +12,8 @@ export const fetchSinglePokemon = async (url) => {
   try {
     const response = await fetch(url);
     const data = await response.json();
-    console.log("single pokemon from api", data);
     return data;
   } catch (error) {
-    console.log(error);
+    throw error;
   }
 };
--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,9 @@ const App = () => {
   const [allPokemon, setPokemon] = useState([]);
   const [singlePokemon, setSinglePokemon] = useState([]);
   const [selectedPokemon, setSelectedPokemon] = useState("");
+  const [abilities, setAbilities] = useState([]);
+  const [stats, setStats] = useState([]);
+  const [sprite, setSprite] = useState("");
 
   useEffect(() => {
     const getAllPokemon = async () => {
@@ -13,16 +16,33 @@ const App = () => {
       );
       setPokemon(getPokemon.results);
     };
+
+    const getPreview = async () => {
+      const preview = await fetchSinglePokemon(
+        "https://pokeapi.co/api/v2/pokemon/1/"
+      );
+
+      setSinglePokemon(preview);
+      setAbilities(preview.abilities);
+      setStats(preview.stats);
+      setSprite(preview.sprites.front_default);
+    };
+
+    getPreview();
     getAllPokemon();
   }, [setPokemon]);
 
   const handleSelect = async (event) => {
     const name = event.target.value;
     setSelectedPokemon(event.target.value);
+
     const singlePoke = await fetchSinglePokemon(
       `https://pokeapi.co/api/v2/pokemon/${name}/`
     );
     setSinglePokemon(singlePoke);
+    setAbilities(singlePoke.abilities);
+    setStats(singlePoke.stats);
+    setSprite(singlePoke.sprites.front_default);
   };
 
   return (
@@ -38,6 +58,32 @@ const App = () => {
         })}
       </select>
       <h2>Selected Pokemon: {selectedPokemon}</h2>
+      <h3>Id Number: {singlePokemon.id}</h3>
+      <h3>
+        Sprite: <img src={sprite} />{" "}
+      </h3>
+      <h3>Base Experience: {singlePokemon.base_experience}</h3>
+      <h3>Height: {singlePokemon.height}"</h3>
+      <h4>{selectedPokemon}'s Stats:</h4>
+      {stats.map((stat) => {
+        return (
+          <>
+            <p>
+              {stat.stat.name}: {stat.base_stat}
+            </p>
+          </>
+        );
+      })}
+      <h4>{selectedPokemon}'s Abilities</h4>
+      {abilities.map((move) => {
+        return (
+          <div>
+            <ul>
+              <li>{move.ability.name}</li>
+            </ul>
+          </div>
+        );
+      })}
     </>
   );
 };
