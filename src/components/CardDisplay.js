15d03643955eb26b17895b1a510f6f99d6261faa
Warren Waterworth <warren.waterworth@gmail.com>
2022-06-13 06:38:04 -0600

    rearranged display card with css

--- src/components/CardDisplay.js
+++ src/components/CardDisplay.js
@@ -3,22 +3,29 @@ import React from "react";
 const CardDisplay = ({ name, types, stats, sprite, id }) => {
   return (
     <div className="card-display">
-      {sprite ? <h4>Pokedex ID# {id}</h4> : null}
-      {sprite ? (
-        <img id="sprite" src={sprite} alt="pokemon default front sprite" />
-      ) : null}
-      <h2>{name.toUpperCase()}</h2>
-      <div>
-        {types &&
-          types.map((type, idx) => {
-            return (
-              <p id="types" key={idx}>
-                {" "}
-                {type.type.name}{" "}
-              </p>
-            );
-          })}
+      <div id="top-card-container">
+        <div id="top-info">
+          <h2>{name.toUpperCase()}</h2>
+          <div id="type-container">
+            {types &&
+              types.map((type, idx) => {
+                return (
+                  <p id="types" key={idx}>
+                    {" "}
+                    {type.type.name}{" "}
+                  </p>
+                );
+              })}
+          </div>
+        </div>
+        <div id="pokedex-id">{sprite ? <h4>Pokedex ID# {id}</h4> : null}</div>
       </div>
+      <div id="sprite-container">
+        {sprite ? (
+          <img id="sprite" src={sprite} alt="pokemon default front sprite" />
+        ) : null}
+      </div>
+
       {!stats ? <h3>Stats:</h3> : null}
       <ul>
         {stats &&
